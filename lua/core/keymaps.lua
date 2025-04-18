73ad85d20bab898a6f775d44c3e10d88f160ced0
yash-yoptima <yash@yoptima.com>
2025-04-18 11:49:53 +0530

    feat: terminal mode in vim
    learn how to exit terminal mode

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -14,3 +14,10 @@ vim.cmd("highlight Normal guibg=NONE")
 vim.cmd("highlight NonText guibg=NONE")
 vim.cmd("highlight Normal ctermbg=NONE")
 vim.cmd("highlight NonText ctermbg=NONE")
+
+vim.keymap.set("n", "<leader>tt", function()
+    vim.cmd.vnew()
+    vim.cmd.term()
+    vim.cmd.wincmd("J");
+    vim.api.nvim_win_set_height(0, 20)
+end)
